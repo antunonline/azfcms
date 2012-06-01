08597c6795cca756447767c979b436161e264127
antun <antun@antun>
2012-06-02 01:21:47 +0200

    Selenium integration

--- tests/Selenium.php
+++ tests/Selenium.php
@@ -15,11 +15,11 @@ class SeleniumTestRunner extends PHPUnit_Extensions_SeleniumTestCase {
     public function testDoh(){
         $this->open("http://azfcms.poljana.vrw/public/js/lib/util/doh/runner.html?testModule=azfcms.tests.modules");
         $this->waitForTextPresent("TEST SUMMARY:");
-        
+        $log = $this->getText("id=logBody");
         $this->takeScreenshot();
-        $this->assertTextPresent("0 errors");
-        $this->assertTextPresent("0 failures");
-        echo $this->getHtmlSource();
+        $this->assertTextPresent("0 errors",$log);
+        $this->assertTextPresent("0 failures",$log);
+        echo $log;
         
     }
     

2829f638f83f4f029bac54db9354ccb01c110be1
antun <antun@antun.(none)>
2012-08-19 13:17:47 +0200

    * Install build script has gained support for build profiles.

--- build/install/build.php
+++ build/install/build.php
@@ -1,125 +1,155 @@
 <?php
-define("DEFINE_BUILD_DEPS",true);
 
-class InstallScriptBuilder{
+define("DEFINE_BUILD_DEPS", true);
+
+class InstallScriptBuilder {
+
     protected $_globals = array();
     protected $_scripts = array();
     protected $_scriptLabels = array();
-    protected $_resources=array();
-    
-    protected $_installScript="";
-    
+    protected $_resources = array();
+    protected $_installScript = "";
+
     /**
      * 
      * @param string $name
      * @param mixed $value
      */
-    public function addVar($name,$value) {
+    public function addVar($name, $value) {
         $this->_globals[$name] = $value;
     }
-    
-    public function addVarFromFile($name,$file) {
-        $this->_globals[$name] = file_get_contents(__DIR__."/".$file);
+
+    public function addVarFromFile($name, $file) {
+        $this->_globals[$name] = file_get_contents(__DIR__ . "/" . $file);
     }
-    
+
     public function addHeader($script) {
         $this->_scripts[] = $script;
     }
-    
+
     public function addFooter($script) {
         $this->_scripts[] = $script;
     }
-    
-    public function addVarFromOpt($name) {
+
+    /**
+     * 
+     * @param string $name
+     * @param mixed $default
+     * @return mixed
+     */
+    public function getOpt($name, $default = null) {
         $argv = $GLOBALS['argv'];
         $searchFor = "--$name";
-        
-        for($i=1,$len=sizeof($argv);$i<$len;$i++){
+
+        for ($i = 1, $len = sizeof($argv); $i < $len; $i++) {
             $arg = $argv[$i];
-            if(strpos($arg, $searchFor)===false){
+            if (strpos($arg, $searchFor) === false) {
                 continue;
             }
-            
-            $this->addVar($name, substr($arg,strpos($arg,"=")+1));
-            return;
+
+            return substr($arg, strpos($arg, "=") + 1);
         }
-        
-        throw new Exception("CLI argument --$name is not provided");
+
+        return $default;
     }
-    
-    
+
+    public function addVarFromOpt($name) {
+        $value = $this->getOpt($name);
+        
+        if (!$value) {
+            if(!isset($this->_globals[$name])){
+                throw new Exception("CLI argument --$name is not provided");
+            }
+            return;
+            
+        } else {
+            $this->_globals[$name] = $value;
+        }
+    }
+
     /**
      * 
      * @param string $script
      */
     public function addScript($displayName, $script) {
-        $scriptPath = __DIR__."/scripts/".$script.".php";
-        if(!file_exists($scriptPath)){
+        $scriptPath = __DIR__ . "/scripts/" . $script . ".php";
+        if (!file_exists($scriptPath)) {
             throw new Exception("Script $script does not exists");
         }
-        
+
         include($scriptPath);
-        
+
         $this->_scripts[] = file_get_contents($scriptPath);
         $this->_scriptLabels[] = $displayName;
     }
-    
-    
-    public function addResource($name,$value) {
+
+    public function addResource($name, $value) {
         $this->_resources[$name] = $value;
     }
-    
-    
+
     /**
      * @return string
      */
     public function buildScript() {
         $scripts = $this->_scripts;
         $header = array_shift($scripts);
-        array_unshift($scripts, "<?php\n\$vars=".var_export($this->_globals, true).";\n?>");
-        array_unshift($scripts, "<?php\n\$resources=".var_export($this->_resources, true).";\n?>");
+        array_unshift($scripts, "<?php\n\$vars=" . var_export($this->_globals, true) . ";\n?>");
+        array_unshift($scripts, "<?php\n\$resources=" . var_export($this->_resources, true) . ";\n?>");
         array_unshift($scripts, $header);
-        
-        return join("",$scripts);
+
+        return join("", $scripts);
     }
-    
+
     public function saveScript($file) {
         $script = $this->buildScript();
         file_put_contents($file, $script);
     }
-    
+
+    protected function _importIniBuildVarGroup($groups) {
+
+        foreach ($groups as $groupName => $group) {
+            if ($groupName == ":vars") {
+                foreach ($group as $key => $value) {
+                    $this->addVar($key, $value);
+                }
+                break;
+            }
+        }
+    }
+
     public function loadIniBuildSchedule() {
-        $buildSchedule = parse_ini_file("build.ini", true);
-        
-        foreach($buildSchedule as $job){
+        $buildScript = $this->getOpt("build", "build");
+        $buildSchedule = parse_ini_file("buildConfigurations/$buildScript.ini", true);
+
+        $this->_importIniBuildVarGroup($buildSchedule);
+
+        foreach ($buildSchedule as $groupName => $job) {
+            if ($groupName[0]==":") {
+                continue;
+            }
             $this->addScript($job['name'], $job['script']);
         }
     }
+
 }
 
-
 class InstallScriptLogger {
+
     protected $_lines = array();
-    
+
     public function writeln($line) {
-        if(is_string($line)){
+        if (is_string($line)) {
             $this->_lines[] = ($line);
-        } else if($line instanceof Exception){
+        } else if ($line instanceof Exception) {
             $this->_lines[] = $line->getMessage();
         } else {
             $this->_lines[] = var_export($line, true);
         }
     }
+
 }
 
 $sb = new InstallScriptBuilder();
-$sb->addVarFromFile("dbDDL", "../resources/DDL.sql");
-$sb->addVarFromFile("dbDML", "../resources/DML.sql");
-$sb->addVarFromOpt("dbHost");
-$sb->addVarFromOpt("dbUser");
-$sb->addVarFromOpt("dbPassword");
-$sb->addVarFromOpt("dbName");
-$sb->addVar("appZipPath","http://web-izgradnja.com/app.zip");
 
 
 $sb->addHeader(file_get_contents("scriptHeader.php"));
--- build/install/scripts/setupDb.php
+++ build/install/scripts/setupDb.php
@@ -2,6 +2,15 @@
 
 if (defined("DEFINE_BUILD_DEPS")) {
     /* @var $this InstallScriptBuilder */
+    
+    $this->addVarFromFile("dbDDL", "../resources/DDL.sql");
+    $this->addVarFromFile("dbDML", "../resources/DML.sql");
+    
+    $this->addVarFromOpt("dbHost");
+    $this->addVarFromOpt("dbUser");
+    $this->addVarFromOpt("dbPassword");
+    $this->addVarFromOpt("dbName");
+    
     return;
 }
 ?><?php
@@ -11,19 +20,17 @@ if (defined("DEFINE_BUILD_DEPS")) {
  */
 $install[] = function(InstallWorkerLog $log, $dbHost, $dbUser, $dbPassword, $dbName, $dbDDL, $dbDML) {
             $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
-
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-            $pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
 
             $stmt = $pdo->query($dbDDL);
-            do{
-                $stmt->fetchAll();
-            }while($stmt->nextRowset());
+            do  {
+                $stmt->closeCursor();
+            } while($stmt->nextRowset());
             
             $stmt = $pdo->query($dbDML);
-            do{
-                $stmt->fetchAll();
-            }while($stmt->nextRowset());
+            do  {
+                $stmt->closeCursor();
+            } while($stmt->nextRowset());
             
             $log->writeln("DB Schema installed");
         };
